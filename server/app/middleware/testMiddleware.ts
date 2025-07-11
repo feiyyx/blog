a8288e0fc479207fec23991f3257e4114d6b84a1
fei yiyang <feiyy2008@qq.com>
2025-07-11 17:40:54 +0800

    feat：http装饰器支持多次传入

--- server/app/middleware/testMiddleware.ts
+++ server/app/middleware/testMiddleware.ts
@@ -1,10 +1,9 @@
-import type { Application, Context } from 'egg';
+import type { Context } from 'egg';
 
-export default function testMiddleware(options: any, app: Application) {
+export default function testMiddleware(options: any) {
     return async (ctx: Context, next: any) => {
         ctx;
-        app;
-        console.log('options: ', options);
+        console.log('options123123123123123123121111111111111111: ', options);
         await next();
     };
 }
